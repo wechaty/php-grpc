c3cf5f0d7f89fb96860acd7e31a50a884a67566a
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-07-12 17:57:23 +0800

    update swoole usage

--- wechaty/Wechaty/PuppetClientSwoole.php
+++ wechaty/Wechaty/PuppetClientSwoole.php
@@ -17,7 +17,7 @@ namespace Wechaty;
 
 /**
  */
-class PuppetClientSwoole extends \Wechaty\Swoole\BaseStub {
+class PuppetClientSwoole extends \Grpc\BaseStub {
 
     /**
      * @param string $hostname hostname
